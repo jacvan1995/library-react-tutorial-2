0d9a7ea41218cc0a31cca3ce917f28161b82e9ca
jacvan1995 <vantassel.je99@gmail.com>
2025-08-18 22:53:24 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
 import {
   BrowserRouter as Router,
+  Routes,
   Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom.min.js";
+} from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Books from "./Pages/Books.jsx";
 
@@ -13,11 +13,11 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/books" component={Books} />
+        <Routes>
+          <Route path="/" exact component={<Home/>} />
+          <Route path="/books" component={<Books/>} />
           <Route path="/cart" />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
     </Router>
